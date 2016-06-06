7e0e63b19ddb89c4378d71e64baccfcb5fe4e603
Viviana Cetrangolo <vcetra87@gmail.com>
2016-06-06 11:10:35 +0200

    started proteincalc

--- dnacalc.py
+++ dnacalc.py
@@ -1,16 +1,15 @@
 #! /usr/bin/env python
 
-# This is a comment
-
-DNASeq = raw_input ( "Enter DNA Sequence:" )
+# DNASeq = raw_input ( "Enter DNA Sequence:" )
+DNASeq = 'ATGATTGTACCCTGACCCTGAT'
 
 DNASeq = DNASeq.upper()
 
 print ('Sequence ' + DNASeq )
 
-SeqLength = float( len ( DNASeq ) )
+SeqLength = float ( len ( DNASeq ) )
 
-print( 'Length is' + str( SeqLength ) )
+print( 'Length is' + str ( SeqLength ) )
 
 
 NumberA = DNASeq.count( 'A' )
@@ -18,7 +17,22 @@ NumberT = DNASeq.count( 'T' )
 NumberG = DNASeq.count( 'G' )
 NumberC = DNASeq.count( 'C' )
 
-print('A: ' + str( NumberA / SeqLength ) )
-print('T: ' + str( NumberT / SeqLength ) )
-print('G: ' + str( NumberG / SeqLength ) )
-print('C: ' + str( NumberC / SeqLength ) )
+
+print ('A: {:.2f}'.format ( NumberA / SeqLength ) )
+print ('T: {:.2f}'.format ( NumberT / SeqLength ) )
+print ('G: {:.2f}'.format ( NumberG / SeqLength ) )
+print ('C: {:.2f}'.format ( NumberC / SeqLength ) )
+
+TotalStrong = NumberG + NumberC
+TotalWeak = NumberA + NumberT
+
+if SeqLength <=14: 
+	MeltTemp = ( 4 * TotalStrong) + (2* TotalWeak )
+	print( "Using long formula." )
+else: 
+	MeltTemp = 64.9 + 41 * (TotalStrong - 16.4) / SeqLength
+	print( "Using long formula." )
+
+print ( ' Melting Temp: {}'.format(MeltTemp) )
+
+print('Done')
